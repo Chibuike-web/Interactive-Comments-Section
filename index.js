f42bd887f04768eecd29df9b2f4a0e4d32b34efe
Maduabuchi Chibuike <juniortechbro@gmail.com>
2024-11-30 19:46:39 +0100

    implemented the delete feature

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const steppers = document.querySelectorAll(".stepper");
 const updateBtns = document.querySelectorAll(".update-btn");
-const cancelBtn = document.querySelector(".cancel-btn"); // modal cancel Button
-const deleteBtn = document.getElementById("delete-btn"); // Comment delete Button
+const cancelBtn = document.getElementById("cancel-btn"); // modal cancel Button
+const deleteBtns = document.querySelectorAll(".delete-btn");
+const modalDeleteBtn = document.getElementById("modal-delete-btn");
 const replyBtn = document.querySelectorAll(".reply-btn");
 
 // Stepper
@@ -91,23 +92,52 @@ editBtns.forEach((editBtn, index) => {
 });
 
 // Comment Delete
-deleteBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	console.log("delete-btn");
-	const overlay = document.querySelector(".overlay");
-	const deleteModal = document.querySelector(".delete-modal");
-	overlay.style.display = "block";
-	deleteModal.style.display = "block";
-	overlay.addEventListener("click", (e) => {
-		if (e.target.classList.contains("overlay")) {
+deleteBtns.forEach((btn, index) => {
+	btn.addEventListener("click", (e) => {
+		e.preventDefault();
+		console.log("delete-btn " + index);
+		const overlay = document.querySelector(".overlay");
+		const deleteModal = document.querySelector(".delete-modal");
+		overlay.style.display = "block";
+		deleteModal.style.display = "block";
+		overlay.addEventListener("click", (e) => {
+			if (e.target.classList.contains("overlay")) {
+				overlay.style.display = "none";
+				deleteModal.style.display = "none";
+			}
+		});
+
+		// Modal cancel button
+		cancelBtn.addEventListener("click", (e) => {
+			e.preventDefault();
 			overlay.style.display = "none";
 			deleteModal.style.display = "none";
-		}
-	});
-	cancelBtn.addEventListener("click", (e) => {
-		e.preventDefault();
-		overlay.style.display = "none";
-		deleteModal.style.display = "none";
+		});
+
+		// Modal Delete button
+		modalDeleteBtn.addEventListener("click", (e) => {
+			e.preventDefault();
+			overlay.style.display = "none";
+			deleteModal.style.display = "none";
+
+			if (index == 0) {
+				const firstComment = document.getElementById("first-comment");
+				firstComment.classList.remove("flex");
+				firstComment.classList.add("hidden");
+			} else if (index == 1) {
+				const secondComment = document.getElementById("second-comment");
+				secondComment.classList.remove("flex");
+				secondComment.classList.add("hidden");
+			} else if (index == 2) {
+				const thirdComment = document.getElementById("third-comment");
+				thirdComment.classList.remove("flex");
+				thirdComment.classList.add("hidden");
+			} else if (index == 3) {
+				const fourthComment = document.getElementById("fourth-comment");
+				fourthComment.classList.remove("flex");
+				fourthComment.classList.add("hidden");
+			}
+		});
 	});
 });
 
